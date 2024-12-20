baf10b57bc2f7935739f3835fb1bcb3359cd9f2b
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2024-12-20 13:34:23 +0100

    creazione repo

---
+++ mail/js/script.js
@@ -0,0 +1,10 @@
+// Mail //
+// Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito 
+// del controllo, utilizzando un ciclo for.
+
+let list = [];
+let email = prompt("Inserisci la tua email");
+
+for () {
+    
+}
