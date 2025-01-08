a10af0239ccec9dc6dda3e1ee22de22f44f8ec16
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-01-08 16:58:05 +0100

    modifiche finali

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 // Gioco dei dadi //
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+let numerocasuale1 = Math.floor(Math.random() * 6) +1;
+console.log (numerocasuale1);
+
+let numerocasuale2 = Math.floor(Math.random() * 6) +1;
+console.log(numerocasuale2);
+
+if (numerocasuale1 > numerocasuale2) {
+    console.log("hai vinto");
+
+} else if(numerocasuale1 == numerocasuale2) {
+
+    console.log("pareggio");
+} else {
+    console.log("hai perso");
+}
--- mail/js/script.js
+++ mail/js/script.js
@@ -2,9 +2,18 @@
 // Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito 
 // del controllo, utilizzando un ciclo for.
 
-let list = [];
+let listaEmail = ["Lorenzo@gmail.com", "Angelica@gmail.com", "Roberto@gmail.com", "Enrica@gmail.com"];
 let email = prompt("Inserisci la tua email");
+let presente = false;
 
-for () {
-    
+for (let i = 0; i < listaEmail.lenght; i++) {
+    if (email === listaEmail[i]) {
+        presente = true;
+    }    
+}
+
+if (presente) {
+    console.log("Puoi entrare");
+} else {
+    console.log("Non puoi entrare");
 }
